f89c971ea2a6bf87b00f9bbba84f9e34397cf99c
Andrew Liu <andrew.a.liu.19@dartmouth.edu>
2018-06-01 16:05:15 -0400

    sa 8 done

---
+++ src/models/note_model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+// create a PostSchema with a title field
+const NoteSchema = new Schema(
+  {
+    title: String,
+    x: Number,
+    y: Number,
+    zIndex: Number,
+    content: String,
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+// create PostModel class from schema
+const NoteModel = mongoose.model('Note', NoteSchema);
+
+export default NoteModel;
